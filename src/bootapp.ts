67711905d8eae19c810c05bb69df7eb336f214ca
undefined <jnjsoft.ko@gmail.com>
2023-07-20 18:45:44 +0900

    bootapp.ts github.ts

--- src/bootapp.ts
+++ src/bootapp.ts
@@ -1,25 +1,61 @@
 #!/usr/bin/env node
 import yargs from "yargs";
-import { execSync } from 'child_process';
-import Path from 'path';
-import { sleep } from "jnj-lib-base-ts/basic.js";
-import { mkdir, cpdir, loadJson, saveJson, saveFile } from "jnj-lib-base-ts/builtin.js";
-import { Github, findGithubAccount } from "jnj-lib-base-ts/github.js";
-import { ENV } from "jnj-lib-base-ts/env.js";
-
-const { TEMPLATES_PATH } = ENV;
+import { execSync } from "child_process";
+import Path from "path";
+import {
+  sleep,
+  mkdir,
+  cpdir,
+  loadJson,
+  saveJson,
+  saveFile,
+  Github,
+  findGithubAccount,
+} from "jnj-lib-base";
+import dotenv from "dotenv";
+dotenv.config(); // 실행 경로에 있는 `.env`
+const templatesPath =
+  process.env.ENV_TEMPLATES_PATH ?? "C:/JnJ-soft/Developments/_Templates";
 
 // * cli options
 const options = yargs
   .usage("Usage: -u <url> -s <keyword>")
-  .option("u", { alias: "userName", default: "mooninlearn", describe: "Name of User", type: "string", demandOption: true })
-  .option("l", { alias: "lang", default: "node", describe: "developemnt language", type: "string" })
-  .option("n", { alias: "repoName", describe: "NameOfRepository", type: "string", demandOption: true })
-  .option("d", { alias: "description", describe: "Description For Repository", type: "string" })
-  .option("g", { alias: "github", default: true, describe: "Use Github Repository", type: "boolean" })
-  .option("t", { alias: "template", default: "none-basic-ts", describe: "App Template", type: "string" })
-  .argv;
-
+  .option("u", {
+    alias: "userName",
+    default: "mooninlearn",
+    describe: "Name of User",
+    type: "string",
+    demandOption: true,
+  })
+  .option("l", {
+    alias: "lang",
+    default: "node",
+    describe: "developemnt language",
+    type: "string",
+  })
+  .option("n", {
+    alias: "repoName",
+    describe: "NameOfRepository",
+    type: "string",
+    demandOption: true,
+  })
+  .option("d", {
+    alias: "description",
+    describe: "Description For Repository",
+    type: "string",
+  })
+  .option("g", {
+    alias: "github",
+    default: true,
+    describe: "Use Github Repository",
+    type: "boolean",
+  })
+  .option("t", {
+    alias: "template",
+    default: "none-basic-ts",
+    describe: "App Template",
+    type: "string",
+  }).argv;
 
 // * init github
 const initGithub = (options) => {
@@ -29,22 +65,25 @@ const initGithub = (options) => {
   const cmd = `github -u ${userName} -e initRepo -n ${repoName} -d "${description}"`;
   execSync(cmd);
   // console.log('initGithub', { name: repoName, description });
-}
+};
 
 // * package.json 업데이트
 const updatePackageJsonForGithub = (userName, repoName, json) => {
-  json = {...json, ...{
-    "repository": {
-      "type": "git",
-      "url": `git+https://${userName}@github.com/${userName}/${repoName}.git`
+  json = {
+    ...json,
+    ...{
+      repository: {
+        type: "git",
+        url: `git+https://${userName}@github.com/${userName}/${repoName}.git`,
+      },
+      bugs: {
+        url: `https://github.com/${userName}/${repoName}/issues`,
+      },
+      homepage: `https://github.com/${userName}/${repoName}#readme`,
     },
-    "bugs": {
-      "url": `https://github.com/${userName}/${repoName}/issues`
-    },
-    "homepage": `https://github.com/${userName}/${repoName}#readme`
-  }}
+  };
   return json;
-}
+};
 
 // ** init app
 // * Nodejs
@@ -53,7 +92,7 @@ const initNode = (options) => {
   const { fullName, email } = findGithubAccount(userName);
 
   // * template 폴더 복사
-  const srcDir = Path.join(TEMPLATES_PATH, `node/${template}`);
+  const srcDir = Path.join(templatesPath!, `node/${template}`);
   const dstDir = Path.join(process.cwd(), `${repoName}`);
 
   cpdir(srcDir, dstDir);
@@ -61,13 +100,15 @@ const initNode = (options) => {
   //* package.json 업데이트
   const path = Path.join(process.cwd(), `${repoName}/package.json`);
   let json = loadJson(path);
-  json = {...json, ...{
-    "name": repoName,
-    "version": "0.0.1", 
-    "description": description, 
-    "author": `${fullName} <${email}>`
-  }};  // version, description, main, author 업데이트
-
+  json = {
+    ...json,
+    ...{
+      name: repoName,
+      version: "0.0.1",
+      description: description,
+      author: `${fullName} <${email}>`,
+    },
+  }; // version, description, main, author 업데이트
 
   // * github
   if (github) {
@@ -83,40 +124,42 @@ const initNode = (options) => {
   // * figma
   // * typescript 인 경우
   const generateRandomCode = (n) => {
-    let str = ''
+    let str = "";
     for (let i = 0; i < n; i++) {
-      str += Math.floor(Math.random() * 10)
+      str += Math.floor(Math.random() * 10);
     }
-    return str
-  }
+    return str;
+  };
 
-  if (template.includes('-figma-')) {
+  if (template.includes("-figma-")) {
     const path = Path.join(process.cwd(), `${repoName}/dist/manifest.json`);
     let json = loadJson(path);
-    json = {...json, ...{
-      name: repoName,
-      id: `1${generateRandomCode(17)}`
-    }}
+    json = {
+      ...json,
+      ...{
+        name: repoName,
+        id: `1${generateRandomCode(17)}`,
+      },
+    };
     saveJson(path, json);
   }
-}
+};
 
 // ** main
 
 // * init github
 if (options.github) {
-  console.log('## init github');
+  console.log("## init github");
   initGithub(options);
-  sleep(5);  // TODO: 필요한지 확인
+  sleep(1); // TODO: 필요한지 확인
 }
 
 // * init app
-switch(options.lang.toUpperCase()) {
-  case 'NODE':
+switch (options.lang.toUpperCase()) {
+  case "NODE":
     initNode(options);
     break;
 }
-
 
 // * syntax
 // bootapp -l node -u jnjsoftko -n jnj-google-api-ts -d "JnJ Library For Google API" -t bare-basic-ts --no-github
--- src/github.ts
+++ src/github.ts
@@ -1,28 +1,54 @@
 #!/usr/bin/env node
-import { Github } from "jnj-lib-base-ts/github.js";
+import { Github } from "jnj-lib-base";
 import yargs from "yargs";
 
 // * cli options
 const options = yargs
   .usage("Usage: -u <url> -s <keyword>")
-  .option("u", { alias: "userName", default: "mooninlearn", describe: "Name of User", type: "string", demandOption: true })
-  .option("e", { alias: "exec", default: "createRepo", describe: "exec command", type: "string", demandOption: true })
-  .option("n", { alias: "repoName", describe: "NameOfRepository", type: "string", demandOption: true })
-  .option("d", { alias: "description", describe: "Description For Repository", type: "string" })
-  .argv;
+  .option("u", {
+    alias: "userName",
+    default: "mooninlearn",
+    describe: "Name of User",
+    type: "string",
+    demandOption: true,
+  })
+  .option("e", {
+    alias: "exec",
+    default: "createRepo",
+    describe: "exec command",
+    type: "string",
+    demandOption: true,
+  })
+  .option("n", {
+    alias: "repoName",
+    describe: "NameOfRepository",
+    type: "string",
+    demandOption: true,
+  })
+  .option("d", {
+    alias: "description",
+    describe: "Description For Repository",
+    type: "string",
+  }).argv;
 
 // * github instance
 const github = new Github(options.userName);
 
 // * exec
-switch(options.exec) {
-  case 'initRepo':
-    github.initRepo({name: options.repoName, description: options.description});
+switch (options.exec) {
+  case "initRepo":
+    github.initRepo({
+      name: options.repoName,
+      description: options.description,
+    });
     break;
-  case 'createRepo':
-    github.createRepo({name: options.repoName, description: options.description});
+  case "createRepo":
+    github.createRepo({
+      name: options.repoName,
+      description: options.description,
+    });
     break;
-  case 'deleteRepo':
-    github.deleteRepo({owner: options.userName, repo: options.repoName});
+  case "deleteRepo":
+    github.deleteRepo({ owner: options.userName, repo: options.repoName });
     break;
 }
